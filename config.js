169af58d7cf790bfa24d7222aaed3df3d8cd5bb8
nicholas <yitaqi2011@gmail.com>
2014-11-14 13:16:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,5 @@
 var config = {
-  limit: 10,
-  secret: 'showfier',
-  cookieMaxAge: 1000*60*60*24*30,
-  cookie_secret: 'showfier',
-  session_secret: 'showfier',
-  cookieName: 'showfie',
-  dbname: 'showfie',
-  dburl: 'mongodb://127.0.0.1/showfie',
-  gender: 'Secret',
-  location: 'Other',
-  profile: 'Not set profile yet!',
-  avatar: 'images/default.jpg',
-  qiniuAccesskey: 'Z8tYMhl1moWh3F14PLb2SSWMrhOHFng6FYinSRcs',
-  qiniuSecretkey: '5fNThsSYpCXQYetJqcGXG8CgjybsdgYDg7FMrmld',
-  qiniuBucket: 'showfie',
-  qiniuDomain: 'http://showfie.qiniudn.com'
+  limit: 10
 }
 
 module.exports = config;
